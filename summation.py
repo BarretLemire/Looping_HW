ab00adf0f3f6d2564eb877219fb3adb5e0efffd2
Barret Lemire <blemire4503@xmail.dixietech.edu>
2024-02-24 11:34:34 -0700

    summation assignments

---
+++ summation.py
@@ -0,0 +1,39 @@
+
+
+def summation_while(n):
+    
+    current_num = 1
+    total_sum = 0
+
+    while current_num <= n:
+
+        total_sum += current_num
+
+        
+    
+        current_num += 1
+
+
+    return total_sum
+
+
+
+
+
+
+def summation_for(n):
+
+    total_sum = 0
+
+    for i in range(1, n+1):
+
+        total_sum += i        
+
+    return total_sum
+
+
+result = summation
+
+
+
+
